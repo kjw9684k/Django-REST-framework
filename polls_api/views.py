909db513615131ac041b405a090a3a8dfd136c91
jongwook <kjw9684k@gmail.com>
2023-04-29 11:44:28 +0900

    Class based Views

--- polls_api/views.py
+++ polls_api/views.py
@@ -5,25 +5,47 @@ from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 # 깃헙 테스트를 위해 코드변경을 해봤습니다.
 # 한번 더 코드변경을 해봤습니다.
 # 한번 정리합니다
-@api_view(['GET', 'POST'])
-def question_list(request):
-    if request.method == 'GET':
+
+class QuestionList(APIView):
+    def get(self, request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many = True)
         return Response(serializer.data)
     
-    if request.method == 'POST':
+    def post(self, request):
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        question = get_object_or_404(Question, pk=id)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
 @api_view(['GET','PUT','DELETE'])
 def question_detail(request, id):
     question = get_object_or_404(Question, pk=id)
