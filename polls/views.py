6211f0276f79ea1f47d2c7c41b4da4909d8b8d14
jongwook <kjw9684k@gmail.com>
2023-04-29 19:43:05 +0900

    Form을 사용하여 User 생성하기

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 # from django.http import Http404
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:15]
@@ -31,3 +34,9 @@ def vote(request, question_id):
 def result(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/result.html', {'question':question})
+
+class SignupView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('user-list')
+    template_name = 'registration/signup.html'
+    
