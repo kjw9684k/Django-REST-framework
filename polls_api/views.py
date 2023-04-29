0cb51776a0af5b624544688b52138c7651e091e5
jongwook <kjw9684k@gmail.com>
2023-04-29 14:40:29 +0900

    generics

--- polls_api/views.py
+++ polls_api/views.py
@@ -4,34 +4,15 @@ from rest_framework.decorators import api_view
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import generics
 
-# 깃헙 테스트를 위해 코드변경을 해봤습니다.
-# 한번 더 코드변경을 해봤습니다.
-# 한번 정리합니다
-
-class QuestionList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class QuestionDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-            return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+  
