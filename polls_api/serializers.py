f17428bb96be38273728eba1c142b18628682481
jongwook <kjw9684k@gmail.com>
2023-04-29 17:05:35 +0900

    User 관리하기

--- polls_api/serializers.py
+++ polls_api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from polls.models import Question
-
+from django.contrib.auth.models import User
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'question_text', 'pub_date']
+
+class UserSerializer(serializers.ModelSerializer):
+    questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'questions']
--- polls_api/views.py
+++ polls_api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 # Create your views here.
 from polls.models import Question
-from polls_api.serializers import QuestionSerializer
+from polls_api.serializers import QuestionSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
@@ -15,4 +16,10 @@ class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-  
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
