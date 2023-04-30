90b2819ee6ca1ce779231fb25ed3a1778c36de64
kjw9684k <kjw9684k@gmail.com>
2023-04-30 17:54:21 +0900

    Votes 2

--- polls_api/views.py
+++ polls_api/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework.decorators import api_view
-# Create your views here.
-from polls.models import Question
-from polls_api.serializers import *
+from polls.models import Question, Vote
+from polls_api.serializers import QuestionSerializer, UserSerializer, RegisterSerializer, VoteSerializer
 from rest_framework.response import Response
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsVoter
+
+class VoteList(generics.ListCreateAPIView):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self, *args, **kwargs):
+        return Vote.objects.filter(voter=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(voter=self.request.user)
+
+class VoteDetail(generics.RetrieveUpdateDestroyAPIView):    
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated, IsVoter]
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
